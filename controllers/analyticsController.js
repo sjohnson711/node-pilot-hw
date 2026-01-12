3b39626828e6a9c14c59b3ae27d4003763dd5ba7
Seth <samaritanbrotherseth@gmail.com>
2026-01-11 22:37:14 -0500

    stopping point before userList and taskcounts

--- controllers/analyticsController.js
+++ controllers/analyticsController.js
@@ -1,37 +1,71 @@
 const prisma = require("../db/prisma");
 const { StatusCodes } = require("http-status-codes");
 
-const userId = parseInt(req.params.id);
+exports.getUserAnalytics = async (req, res) => {
+  const userId = parseInt(req.params.id);
+  if (isNaN(userId)) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: "Invalid Id format" });
+  }
 
-if (isNAN(userId)) {
-  return res
-    .status(StatusCodes.BAD_REQUEST)
-    .json({ message: "Invalid Id format" });
-}
+  //Check if user exist
+  const user = await prisma.user.findUnique({
+    where: { id: userId },
+  });
 
-//Use groupBy to count tasks by completion status
-const taskStats = await prisma.task.groupBy({
-  by: ["isCompleted"],
-  where: { userId },
-  _count: {
-    id: true,
-  },
-});
+  if (!user) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ message: "User Not Found" });
+  }
 
-//Include recent task activity with eager loading
-const recentTasks = await prisma.task.findMany({
-  where: { userId },
-  select: {
-    id: true,
-    title: true,
-    isCompleted: true,
-    priority: true,
-    createdAt: true,
-    userId: true,
-    User: {
-      select: { name: true },
+  //Use groupBy to count tasks by completion status
+  const taskStats = await prisma.task.groupBy({
+    by: ["isCompleted"],
+    where: { userId },
+    _count: {
+      id: true,
     },
-  },
-  orderBy: { createdAt: 'desc'},
-  take: 10
-});
+  });
+
+  //Include recent task activity with eager loading
+  const recentTasks = await prisma.task.findMany({
+    where: { userId },
+    select: {
+      id: true,
+      title: true,
+      isCompleted: true,
+      priority: true,
+      createdAt: true,
+      userId: true,
+      User: {
+        select: { name: true },
+      },
+    },
+    orderBy: { createdAt: "desc" },
+    take: 10,
+  });
+
+  //Calculate weekly progress using groupBy
+  //first, calculate the date from one week ago
+  //hint, Use new Date() and setDate() - 7
+
+  const oneWeekAgo = new Date();
+  oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+
+  //use groupBy with a where clause filtering by createdAt>= oneWeekAgo
+  const weeklyProgress = await prisma.task.groupBy({
+    by: ["createdAt"],
+    where: {
+      userId,
+      createdAt: { gte: oneWeekAgo },
+    },
+    _count: { id: true },
+  });
+
+  //return response with taskStats, recentTasks, and weeklyProgress
+  return res
+    .status(StatusCodes.OK)
+    .json({ taskStats, recentTasks, weeklyProgress });
+};
