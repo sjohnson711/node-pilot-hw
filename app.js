9321a15ef43996fc373bcb7606673b0e19558c89
Seth <samaritanbrotherseth@gmail.com>
2026-01-18 23:41:46 -0500

    added new files and replaced global.user_id with req.user.id using the jwt authentication to authetincate the user in the cookie to match the header. left off at postman portion of the assignment

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const app = express();
 app.use(express.json({ limit: "1kb" })); //parsing the body of the request json
-const authMiddleware = require("./middleware/auth");
+const jwtMiddleware = require("./middleware/jwtMiddleware");
 const prisma = require("./db/prisma");
 const analyticsRoutes = require('./routes/analyticsRoutes')
+const cookieParser = require("cookie-parser");
+app.use(cookieParser())
 
 const taskRouter = require("./routes/taskRoutes");
-app.use("/api/tasks", authMiddleware, taskRouter); // -----> Auth get called so that if the user is not loggeeon they cannot access the tasks.
+app.use("/api/tasks", jwtMiddleware, taskRouter); // -----> Auth get called so that if the user is not loggeeon they cannot access the tasks.
 
-global.user_id = null;
-global.users = []; //-------> Array of objects of users
-global.tasks = []; //-------> using this in my taskController.js
 
 const middleFunction = (req, res, next) => {
   console.log(
